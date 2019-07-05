83c7525306443ba4fe0b307e291075f04d412753
tdhai <haitruong1405@gmail.com>
2019-07-05 13:58:54 +0700

    kafka update

--- server/controllers/cutomerController.js
+++ server/controllers/cutomerController.js
@@ -1,81 +1,86 @@
-const service = require('../services/customerService')
-const models = require('../models/customerModel')
-const producer = require('../kafka/producer')
-const consumer = require('../kafka/consumer')
+const service = require("../services/customerService");
+const models = require("../models/customerModel");
+const producer = require("../kafka/producer");
+const consumer = require("../kafka/consumer");
 
 const createAccount = async (req, h) => {
   try {
-    const cusName = req.payload.name
-    const cusEmail = req.payload.email
-    const cusPassword = req.payload.password
-    const cusRePassword = req.payload.rePassword
-    return await service.createAccount(cusEmail, cusName, cusPassword, cusRePassword);
+    const cusName = req.payload.name;
+    const cusEmail = req.payload.email;
+    const cusPassword = req.payload.password;
+    const cusRePassword = req.payload.rePassword;
+    return await service.createAccount(
+      cusEmail,
+      cusName,
+      cusPassword,
+      cusRePassword
+    );
   } catch (err) {
-    return ("create account fail CONTROLLER", err)
-  };
-}
+    return "create account fail CONTROLLER", err;
+  }
+};
 
 const getAllCustomers = async (req, h) => {
   try {
     return service.getAllCustomers();
   } catch (err) {
-    return ("get all customer fail CONTROLLER", err)
-  };
+    return "get all customer fail CONTROLLER", err;
+  }
 };
 
 const login = async (req, h) => {
   try {
-<<<<<<< HEAD
-    const a =req.payload.email
+    const a = req.payload.email;
     // await producer.send(a)
     // await consumer
 
-    producer.sendMessage()
+    producer.sendMessage();
 
-=======
-    // console.log("ASDASSAAAAAAAAAAAAAAAAAAAAAAAAaaaa")
-    // producer.send(req.payload.email)
->>>>>>> fefc4e79b9f2011dddb349d871bd661c67e64abe
-    const cusEmail = req.payload.email
-    const cusPassword = req.payload.password
-    
-    return await service.login(cusEmail, cusPassword)
+    const cusEmail = req.payload.email;
+    const cusPassword = req.payload.password;
+
+    return await service.login(cusEmail, cusPassword);
   } catch (err) {
-    throw err
-  };
-}
+    throw err;
+  }
+};
 
 const getCustomer = async (req, h) => {
   try {
-    const customerID = req.params.id
-    return await service.getCustomer(customerID)
+    const customerID = req.params.id;
+    return await service.getCustomer(customerID);
   } catch (err) {
-    return ("get customer fail CONTROLLER", err)
+    return "get customer fail CONTROLLER", err;
   }
-}
+};
 
 const updateName = async (req, h) => {
   try {
-    const cusName = req.payload.name
-    const id = req.auth.credentials.data
-    console.log(id, cusName)
+    const cusName = req.payload.name;
+    const id = req.auth.credentials.data;
+    console.log(id, cusName);
     return await service.updateName(id, cusName);
   } catch (err) {
-    return ("update account fail CONTROLLER", err)
+    return "update account fail CONTROLLER", err;
   }
-}
+};
 
 const updatePassword = async (req, h) => {
   try {
-    const passwordNew = req.payload.passwordNew
-    const rePasswordNew = req.payload.rePasswordNew
-    const passwordOld = req.payload.passwordOld
-    const id = req.auth.credentials.data
-    return await service.updatePassword(id, passwordOld, passwordNew, rePasswordNew);
+    const passwordNew = req.payload.passwordNew;
+    const rePasswordNew = req.payload.rePasswordNew;
+    const passwordOld = req.payload.passwordOld;
+    const id = req.auth.credentials.data;
+    return await service.updatePassword(
+      id,
+      passwordOld,
+      passwordNew,
+      rePasswordNew
+    );
   } catch (err) {
-    return ("update account fail CONTROLLER", err)
+    return "update account fail CONTROLLER", err;
   }
-}
+};
 
 module.exports = {
   createAccount,
@@ -84,4 +89,4 @@ module.exports = {
   updateName,
   updatePassword,
   getCustomer
-}
+};
--- server/kafka/consumer.js
+++ server/kafka/consumer.js
@@ -1,5 +1,5 @@
-// var Kafka = require("node-rdkafka");
-// require('dotenv').config();
+var Kafka = require("node-rdkafka");
+require("dotenv").config();
 
 exports.plugin = {
   register: (server, option) => {
@@ -11,33 +11,29 @@ exports.plugin = {
       "sasl.mechanisms": "SCRAM-SHA-256",
       "sasl.username": process.env.CLOUDKARAFKA_USERNAME,
       "sasl.password": process.env.CLOUDKARAFKA_PASSWORD,
-      "debug": "generic,broker,security",
+      debug: "generic,broker,security",
       "enable.partition.eof": false
     };
 
     const prefix = process.env.CLOUDKARAFKA_TOPIC_PREFIX;
     const topics = [`${prefix}order`];
-    console.log(topics)
+    console.log(topics);
     const consumer = new Kafka.KafkaConsumer(kafkaConf);
     const numMessages = 5;
     let counter = 0;
 
+    consumer.connect(console.log("abc"));
 
-    consumer.connect(
-      console.log("abc")
-    );
-
-    consumer.on("error", function (err) {
+    consumer.on("error", function(err) {
       console.error(err);
     });
-    consumer.on("ready", function (arg) {
+    consumer.on("ready", function(arg) {
       console.log(`Consumer ${arg.name} ready`);
       consumer.subscribe(topics);
       consumer.consume();
     });
 
-
-    consumer.on("data", function (m) {
+    consumer.on("data", function(m) {
       console.log("ab");
       console.log("consumer data", m);
       counter++;
@@ -48,30 +44,23 @@ exports.plugin = {
       console.log(m.value.toString());
     });
 
-
-
-
-    consumer.on("disconnected", function (arg) {
+    consumer.on("disconnected", function(arg) {
       // process.exit();
     });
 
-
-    consumer.on('event.error', function (err) {
+    consumer.on("event.error", function(err) {
       console.error(err);
       process.exit(1);
     });
 
-
-
-    consumer.on('event.log', function (log) {
+    consumer.on("event.log", function(log) {
       // console.log(log);
     });
-
 
     // setTimeout(function () {
     //   console.log("Disconect")
     //   consumer.disconnect();
     // }, 10000);
   },
-  name: 'Consumer'
-}
+  name: "Consumer"
+};
